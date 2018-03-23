4819b18790c90e9aee12cb01a9bbf6c729342f14
Yu-Jin <yujinost@gmail.com>
2018-03-23 18:28:36 +0000

    first attempt

--- PalindromeChecker/Program.cs
+++ PalindromeChecker/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PalindromeChecker
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(IsPalindrome("racecar"));
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            bool isItTrue = false;
+
+            for(int i=0; i<input.Length; i++)
+            {
+                char lastChar = input[input.Length - 1 - i];
+                char firstChar = input[i];
+
+                //Console.Write(lastChar);
+                //Console.Write(firstChar);
+
+                if(firstChar == lastChar)
+                {
+                    isItTrue = true;
+                }
+                else
+                {
+                    isItTrue = false;
+                    break;
+                }
+            }
+            return isItTrue;
         }
     }
 }
